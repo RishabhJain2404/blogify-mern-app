cbd42a348841a188d6d1ee3fc8eed5b7708805f7
Rishabh Jain <rishabh2404jain@gmail.com>
2024-03-28 17:20:02 +0530

    Created and added the footer component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         <Link to ="/" className='self-center whitespace-nowrap text-sm 
         sm:text-xl font-semibold dark:text-white'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Rishabh's</span>
-            Blog
+            Blogify
         </Link>
         <form>
             <TextInput
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -45,7 +45,7 @@ export default function SignUp() {
         <Link 
           to ="/" className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Rishabh's</span>
-            Blog
+            Blogify
         </Link>
         <p className='text-sm mt-5'>
             This is a demo project. You can sign up with your email and password
