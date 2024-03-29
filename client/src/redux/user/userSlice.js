097f60b036128eeaf54df5ad5f6167dede966d44
Rishabh Jain <rishabh2404jain@gmail.com>
2024-03-30 04:57:45 +0530

    Completed update user profile page functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,6 +23,19 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   }
 });
 
@@ -30,6 +43,9 @@ export const {
   signInStart,
   signInSuccess,
   signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
